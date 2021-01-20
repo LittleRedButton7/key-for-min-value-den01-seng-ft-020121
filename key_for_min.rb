465ac7cbbf46b865bbe5448a91f744f2cd719763
Marissa Nolan <ritz1484@cs.com>
2021-01-20 21:32:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  num_to_compare_to = 0
+  num_to_compare_to = 10000000
   name_hash.each do |key, value|
     if value < num_to_compare_to
       num_to_compare_to = value
